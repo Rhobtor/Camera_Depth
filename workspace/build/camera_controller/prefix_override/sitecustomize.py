1759ed69b2f88514d68c3486fbf1bca53d79815c
rhobtor <luismidp7@gmail.com>
2024-01-23 10:48:08 +0100

    update files

--- workspace/build/camera_controller/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/root/Luis/Camera_Depth/install/camera_controller'
---
+++ zed_workspace/src/camera_controller/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'camera_controller'
+submodule= 'camera_controller/submodulos'
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rhobtor',
+    maintainer_email='rhobtor@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'distance=camera_controller.distance:main',
+            'distance_move=camera_controller.distance_move:main',
+            'distance_cells=camera_controller.distnce_move:main',
+            'distance_all=camera_controller.distance_all:main',
+        ],
+    },
+)
